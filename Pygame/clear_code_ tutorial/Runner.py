09ff5139d8e0d017b14dcc175cb881b24272db14
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-07-13 17:08:37 -0300

    adição de sons, jogo finalizado

--- Pygame/clear_code_ tutorial/Runner.py
+++ Pygame/clear_code_ tutorial/Runner.py
@@ -16,10 +16,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom = (80, 300))
         self.gravity = 0
 
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.3)
+
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
     
     def apply_gravity(self):
         self.gravity += 1
@@ -94,6 +98,9 @@ clock = pygame.time.Clock()
 game_active = False
 start_time = 0
 test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.set_volume(0.2)
+bg_music.play(loops = -1)
 
 # groups:
 player = pygame.sprite.GroupSingle()
