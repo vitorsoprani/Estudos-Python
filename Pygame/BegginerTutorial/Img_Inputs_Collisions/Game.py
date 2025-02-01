04251dcd80c0f8dc091ed28e2dd5fae2a47deb30
Vitor S. Passamani <vitor.spassamani@gmail.com>
2025-02-01 16:11:28 -0300

    alterações no codigo

--- Pygame/BegginerTutorial/Img_Inputs_Collisions/Game.py
+++ Pygame/BegginerTutorial/Img_Inputs_Collisions/Game.py
@@ -19,9 +19,37 @@ player_position = [50,50]
 player_y_momentum = 0
 player_rect = pygame.Rect(player_position[0], player_position[1], player_img.get_width(), player_img.get_height())
 
-plataform_1 = pygame.Rect(0, 200, 150, 20)
+plataform_1 = pygame.Rect(100, 100, 100, 100)
 
 while True:
+    screen.fill("purple")
+
+    if player_rect.colliderect(plataform_1):
+        pygame.draw.rect(screen, "red", plataform_1)
+    else:
+        pygame.draw.rect(screen, "yellow", plataform_1)
+    
+    screen.blit(player_img, player_position)
+    
+    if player_position[1] > screen.get_height() - player_img.get_height():
+        player_y_momentum = - player_y_momentum
+    else:
+        player_y_momentum += 0.5
+    player_position[1] += player_y_momentum
+    
+    if player_position[0] > screen.get_width():
+        player_position[0] = - player_img.get_width()
+    if player_position[0] < - player_img.get_width():
+        player_position[0] = screen.get_width() 
+    
+    if moving_left:
+        player_position[0] -= 4
+    if moving_right:
+        player_position[0] += 4
+    
+    player_rect.x = player_position[0]
+    player_rect.y = player_position[1]
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -38,38 +66,7 @@ while True:
                 moving_right = False
             if event.key == pygame.K_LEFT:
                 moving_left = False
-    screen.fill("purple")
 
-    if player_y_momentum < 15:
-        player_y_momentum += 0.5
     
-    if player_position[1] > screen.get_height() - player_img.get_height():
-        player_y_momentum = - player_y_momentum
-    
-    if player_rect.colliderect(plataform_1):
-        if player_y_momentum > 0: player_y_momentum = -15
-        else: player_y_momentum = 15
-
-
-    player_position[1] += player_y_momentum
-
-    if player_position[0] > screen.get_width():
-        player_position[0] = - player_img.get_width()
-    if player_position[0] < - player_img.get_width():
-        player_position[0] = screen.get_width() 
-    
-    if moving_left:
-        player_position[0] -= 4
-    
-    if moving_right:
-        player_position[0] += 4
-
-    player_rect.x = player_position[0]
-    player_rect.y = player_position[1]
-
-    screen.blit(player_img, player_position)
-    pygame.draw.rect(screen,"red", plataform_1)
-
     pygame.display.update()
-
     clock.tick(60)
